5dca75a7967a74bb6a657b65e2dc3b33001b2744
Ismail030104 <Berraismail5@gmail.com>
2026-02-10 11:39:21 +0700

    Add files via upload

---
+++ Icons.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { 
+  BookOpen, 
+  PenTool, 
+  Camera, 
+  Upload, 
+  ChevronLeft, 
+  ArrowRight, 
+  CheckCircle, 
+  AlertCircle,
+  FileText,
+  Download,
+  Info,
+  CreditCard,
+  User,
+  Star
+} from 'lucide-react';
+
+export const Icons = {
+  Book: BookOpen,
+  Grade: PenTool,
+  Camera: Camera,
+  Upload: Upload,
+  Back: ChevronLeft,
+  Next: ArrowRight,
+  Check: CheckCircle,
+  Alert: AlertCircle,
+  File: FileText,
+  Download: Download,
+  Info: Info,
+  Pay: CreditCard,
+  Profile: User,
+  Star: Star
+};
